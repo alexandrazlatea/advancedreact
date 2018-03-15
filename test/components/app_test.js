de4231d55f94264814d41ab73da7ae9c976d98cf
Zlatea Alexandra <zlatea.alexandra@gmail.com>
2018-03-16 00:02:58 +0100

    first commit

--- test/components/app_test.js
+++ test/components/app_test.js
@@ -1,14 +1,25 @@
-import { renderComponent , expect } from '../test_helper';
+import {renderComponent, expect} from '../test_helper';
 import App from '../../src/components/app';
+import CommentBox from "../../src/components/comment_box";
 
-describe('App' , () => {
-  let component;
+// use describe to group together similar tests
+describe('Test App Component', () => {
+    let component;
+    beforeEach(() => {
+        component = renderComponent(App);
+    });
+ it('shows a comment box', () => {
+     expect(component.find('.comment-box')).to.exist;
+ });
 
-  beforeEach(() => {
-    component = renderComponent(App);
-  });
+ it ('shows a comment list', () => {
+     expect(component.find('.comment-list')).to.exist;
+ })
 
-  it('renders something', () => {
-    expect(component).to.exist;
-  });
-});
+})
+
+
+
+
+
+
